7feae28d9e7c750223d12c7e332d20d45e99e1d1
Ethan-23 <emanroberts@gmail.com>
2021-12-16 23:16:03 +0000

    made sure last node ended with null

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -21,6 +21,7 @@ listint_t *insert_node(listint_t **head, int number)
     if (new_node == NULL)
         return NULL;
     new_node->n = number;
+    new_node->next = NULL;
     if (number <= new_list->n)
     {
         new_node->next = *head;
