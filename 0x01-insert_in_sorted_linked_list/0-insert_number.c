d7795f6cc690cbe5dfad08fb90ab145c632b143c
Ethan-23 <emanroberts@gmail.com>
2021-12-16 22:44:47 +0000

    added check for begining input

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -17,6 +17,12 @@ listint_t *insert_node(listint_t **head, int number)
     if (new_node == NULL || new_list == NULL || head == NULL)
         return NULL;
     new_node->n = number;
+    if (number < new_list->n)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return (*head);
+    }
     while(new_list->next != NULL)
     {
         new_list = new_list->next;
