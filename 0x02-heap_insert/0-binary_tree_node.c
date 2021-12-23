7c3f5ad5c74d25698267fc583d7102d2c9e8a558
Ethan-23 <emanroberts@gmail.com>
2021-12-23 17:48:50 +0000

    betty changes

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -5,16 +5,17 @@
  * binary_tree_node - inserts node to binary tree
  * @parent: Parent node
  * @value: Value to input to node
+ * Return: New Node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if (new_node == NULL)
-        return NULL;
-    new_node->parent = parent;
-    new_node->n = value;
-    new_node->right = NULL;
-    new_node->left = NULL;
-    return(new_node);
+	if (new_node == NULL)
+		return (NULL);
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->right = NULL;
+	new_node->left = NULL;
+	return (new_node);
 }
