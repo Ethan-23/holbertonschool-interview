fccbc14227ae2864a8b4c9d5c81a9d5013a18bb5
Ethan-23 <emanroberts@gmail.com>
2022-01-05 17:48:47 +0000

    added task 0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""0. Minimum Operations"""
+
+
+def minOperations(n):
+    """
+    Finds the least amount of steps to reach n starting from 1
+    Copy all / Paste
+    Returns an integer
+    """
+
+    if n <= 0:
+        return 0
+    if type(n) != int:
+        return 0
+    step = 1
+    increase = 1
+    i = 1
+    while i < n:
+        if n % i == 0 and i < n/2:
+            step += 1
+            increase += 1
+        i += increase
+        step += 1
+    return step
