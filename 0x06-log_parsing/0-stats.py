c0a05a8e84e39c4c912e9a3ea245bde4ac89baa1
Ethan-23 <emanroberts@gmail.com>
2022-01-28 02:11:22 +0000

    initial commit

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Log parsing"""
+
+import sys
+
+
+def printValues(calls, file_size):
+    codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    print("File Size: {}".format(file_size))
+    for i in range(len(codes)):
+        if calls[i] > 0:
+            print("{}: {}".format(codes[i], calls[i]))
+
+
+codes = [200, 301, 400, 401, 403, 404, 405, 500]
+calls = [0, 0, 0, 0, 0, 0, 0, 0]
+lines = 0
+file_size = 0
+try:
+    for line in sys.stdin:
+        try:
+            lines += 1
+            split = line.split()
+            file_size += int(split[-1])
+            code = int(split[-2])
+            calls[codes.index(code)] += 1
+            if lines == 10:
+                lines = 0
+                printValues(calls, file_size)
+        except Exception:
+            continue
+except Exception:
+    pass
+finally:
+    printValues(calls, file_size)
