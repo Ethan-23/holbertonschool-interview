ab21a6edd413af02d8f9d68b10c744352cfccae8
Ethan-23 <emanroberts@gmail.com>
2022-02-17 18:50:57 +0000

    Undo last commit and checks if binary > 8 bits

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -7,16 +7,14 @@ def validUTF8(data):
     data - incoming list of integers to validate
     Return: True if data is a valid UTF-8 encoding, else return False
     """
-    if data == None:
-        return False
     check = 0
     for i in range(len(data)):
         binary = toBinary(data[i])
+        if len(binary) > 8:
+            return False
         if check == 0:
             while binary[check] == '1':
                 check += 1
-            if check > 4:
-                return False
         else:
             if binary[0] != '1' and binary[1] != '0':
                 return False
