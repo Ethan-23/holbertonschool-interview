e92c78739b79a2f3b6043aa4f189a885c744ecc7
Ethan-23 <emanroberts@gmail.com>
2022-02-17 19:07:53 +0000

    Corrected checking for extra byte again

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -13,8 +13,10 @@ def validUTF8(data):
         if len(binary) > 8:
             return False
         if check == 0:
-            while binary[check + 1] == '1':
+            while binary[check] == '1':
                 check += 1
+            if check > 1:
+                check -= 1
         else:
             if binary[0] == '1' and binary[1] == '0':
                 check -= 1
