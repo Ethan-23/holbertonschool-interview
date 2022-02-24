ad0756b5077e9118358bddd96de0d1d9e284bd4b
Ethan-23 <emanroberts@gmail.com>
2022-02-24 02:38:06 +0000

    added task

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef LISTS_H
+#define LISTS_H
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT 2
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int slide_line(int *line, size_t size, int direction);
+
+
+#endif /* LISTS_H */
