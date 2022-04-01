76b9b144f08c5f1fd96c755236f8dc6cbc5448d6
Ethan-23 <emanroberts@gmail.com>
2022-04-01 00:48:38 +0000

    recode to fit for betty and optimize

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -12,47 +12,34 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *skip = list;
 	skiplist_t *prev = list;
-	skiplist_t *temp;
 
 	if (list == NULL)
 		return (NULL);
 	skip = skip->express;
-	while (skip)
+	while (skip->next)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
 			   skip->index, skip->n);
-		if (skip->n > value || skip->express == NULL)
+		if (skip->n >= value || skip->express == NULL)
 		{
-			if (skip->express == NULL)
+			if (skip->express == NULL && !(skip->n >= value))
 			{
-				temp = skip;
+				prev = skip;
 				while (skip->next)
-				{
 					skip = skip->next;
-					if (skip->n > value)
-					{
-						skip = temp;
-						break;
-					}
-				}
-				if (prev->express->index < skip->index)
-					prev = prev->express;
 			}
 			printf("Value found between indexes [%lu] and [%lu]\n",
 				   prev->index, skip->index);
-			while (prev->n != value)
+			while (prev)
 			{
 				printf("Value checked at index [%lu] = [%d]\n",
 					   prev->index, prev->n);
-				if (prev->next == NULL)
-					return (NULL);
+				if (prev->n == value)
+					return (prev);
 				prev = prev->next;
 			}
-			printf("Value checked at index [%lu] = [%d]\n",
-					prev->index, prev->n);
-			return (prev);
 		}
-		else if (skip->n < value)
+		else
 		{
 			prev = skip;
 			skip = skip->express;
