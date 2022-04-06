38a6cd3f3ad1b5a2ab19e988988f4a14dc75f75c
Ethan-23 <emanroberts@gmail.com>
2022-04-06 05:26:55 +0000

    Corrected order of get

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,19 +1,26 @@
 #!/usr/bin/node
-const myArgs = process.argv[2];
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs;
-request(url, (error, response, body) => {
-  if (error) console.log(error);
-  const json = JSON.parse(body);
+async function main () {
+  const myArgs = process.argv[2];
+  const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs;
+  const json = JSON.parse(await doRequest(url));
   let i = 0;
   for (i in json.characters) {
-    getCharacter(json.characters[i]);
+    const chars = await doRequest(json.characters[i]);
+    console.log(JSON.parse(chars).name);
   }
-});
+}
 
-async function getCharacter (url) {
-  await request(url, (error, response, body) => {
-    if (error) console.log(error);
-    console.log(JSON.parse(body).name);
+function doRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
   });
 }
+
+main();
