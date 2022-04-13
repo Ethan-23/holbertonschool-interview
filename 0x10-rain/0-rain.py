581f19ef4d68fc0e994a0cf7004ee02b7397fa13
Ethan-23 <emanroberts@gmail.com>
2022-04-13 20:52:14 +0000

    added extra cese

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -8,7 +8,9 @@ def rain(walls):
     (amount of spaces between numbers in array width & height)
     """
     amount = 0
+    
     for i in range(len(walls)):
+        sub = 0
         if walls[i] != 0:
             for j in range(i, len(walls)):
                 if j != i and walls[j] != 0:
@@ -16,6 +18,10 @@ def rain(walls):
                         low = walls[j]
                     else:
                         low = walls[i]
-                    amount += low * (j - i - 1)
+                    check = low * (j - i - 1)
+                    if check == 0:
+                        sub += walls[j]
+                        continue    
+                    amount += check - sub
                     break
     return amount
