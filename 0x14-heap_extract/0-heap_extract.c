f21e7ba123d8f4db2300f229e452643ca4ca7dba
Ethan-23 <emanroberts@gmail.com>
2022-05-13 03:39:17 +0000

    pushed files

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extracts the root of a give heap
+ * @root: given root node to extract
+ * Return: new root / 0 on fail
+ */
+
+int heap_extract(heap_t **root)
+{
+    if(root == NULL)
+        return(0);
+    return(0);
+}
