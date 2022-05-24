6e685a4566944aac925489cf9eab62a11b2cbc8d
Ethan-23 <emanroberts@gmail.com>
2022-05-24 20:12:34 +0000

    first commit

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Rotate 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates given matrix 90 degrees"""
+    startcol = 0
+    startrow = len(matrix) - 1
+    temp = []
+    copy = []
+    for i in range(len(matrix)):
+        temp = []
+        for j in range(len(matrix[0])):
+            temp.append(matrix[i][j])
+        copy.append(temp)
+    for row in range(len(matrix)):
+        if row != 0:
+            startrow = 2
+            startcol += 1
+        for col in range(len(matrix[0])):
+            matrix[row][col] = copy[startrow][startcol]
+            startrow -= 1
