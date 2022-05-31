a1fc43a02a330499c4f7da24855fed3160f18fa9
Ethan-23 <emanroberts@gmail.com>
2022-05-31 19:28:02 +0000

    added inner loop to get to correct head of loop

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -7,18 +7,25 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *one = head, *two = head;
+	listint_t *one = head, *two = head;
 
-    if(head == NULL || head->next == NULL)
-        return(NULL);
-    while(one->next != NULL && two->next->next != NULL)
-    {
-        one = one->next;
-        two = two->next->next;
-        if(one == two)
-        {
-            return(one);
-        }
-    }
-    return(NULL);
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+	while (one->next != NULL && two->next->next != NULL)
+	{
+		one = one->next;
+		two = two->next->next;
+		if (one == two)
+		{
+			one = head;
+			while (two->next != NULL)
+			{
+				if (one == two)
+					return (one);
+				one = one->next;
+				two = two->next;
+			}
+		}
+	}
+	return (NULL);
 }
