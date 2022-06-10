1351b0241b50e335ab2ca6207eac1d7cf820e11c
Ethan-23 <emanroberts@gmail.com>
2022-06-10 02:03:12 +0000

    Reorganized code into functions and corrected solutions with new algorithm

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -4,6 +4,50 @@
 import sys
 
 
+def printSolution(board):
+    """printSolution"""
+    result = []
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == 1:
+                result.append([i, j])
+    print(result)
+
+
+def isSafe(board, row, col):
+    """isSafe"""
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    (i, j) = (row, col)
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i = i - 1
+        j = j - 1
+
+    (i, j) = (row, col)
+    while i >= 0 and j < len(board):
+        if board[i][j] == 1:
+            return False
+        i = i - 1
+        j = j + 1
+    return True
+
+
+def nQueen(board, row):
+    """nQueen"""
+    if row == len(board):
+        printSolution(board)
+        return
+    for i in range(len(board)):
+        if isSafe(board, row, i):
+            board[row][i] = 1
+            nQueen(board, row + 1)
+            board[row][i] = 0
+
+
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
@@ -13,36 +57,7 @@ if not str.isdigit(sys.argv[1]):
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
+n = int(sys.argv[1])
 
-number = int(sys.argv[1])
-counter = 0
-allspots = []
-checkspots = []
-prev = -1
-
-while len(allspots) < number - 2:
-    cols = []
-    rows = []
-    spots = []
-    snum = number / 2
-    if number % 2 != 0:
-        snum = (number - 2) / 2 + 1
-    for col in range(number):
-        for row in range(number):
-            if col == row or col in cols or row in rows or \
-               [col, row] in checkspots or [row, col] in spots:
-                continue
-            if prev + 2 + counter <= number - 1:
-                if prev + 2 + counter != row:
-                    continue
-            if counter >= snum:
-                if [row, col] not in checkspots:
-                    continue
-            rows.append(row)
-            cols.append(col)
-            spots.append([col, row])
-            checkspots.append([col, row])
-            prev = row
-    print(spots)
-    allspots.append(spots)
-    counter += 1
+board = [[0 for x in range(n)] for y in range(n)]
+nQueen(board, 0)
