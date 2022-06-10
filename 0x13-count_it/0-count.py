197dbd5e88bbc861a0b330fb27b2bb0fcf948bac
Ethan-23 <emanroberts@gmail.com>
2022-06-10 00:39:12 +0000

    repush

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -31,10 +31,7 @@ def count_words(subreddit, word_list, after='null', word_count=[]):
     if after not in ["NULL", 'null', None, "None"]:
         return count_words(subreddit, word_list, after, word_count)
     else:
-        word_dict = {}
-        for i in word_count:
-            if i not in word_dict.keys():
-                word_dict[i] = word_count.count(i)
+        word_dict = {i: word_count.count(i) for i in word_count}
         word_dict = {k: v for k, v in sorted(word_dict.items(),
                                              key=lambda item: item[1],
                                              reverse=True)}
