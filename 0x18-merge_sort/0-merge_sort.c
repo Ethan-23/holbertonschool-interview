18fccc637f6683cde44ae42f044248328a0b0f5a
Ethan-23 <emanroberts@gmail.com>
2022-06-10 00:08:51 +0000

    added files

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,13 @@
+#include <sort.h>
+
+/**
+ * merge_sort - sorts the given array
+ * @array: Given array
+ * @size: size of split arrays
+ * Return: void
+ */
+
+void merge_sort(int *array, size_t size)
+{
+
+}
---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void merge_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif
