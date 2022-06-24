08f3edff7cf8d86a196d0af1871e4ea6ea5d2554
Ethan-23 <emanroberts@gmail.com>
2022-06-24 02:26:34 +0000

    test push

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -5,11 +5,7 @@ Uses given list of coins to calculate how many you need to reach the total
 
 
 def makeChange(coins, total):
-    """
-    makeChange
-    coins: List of given coins
-    total: Total that needs to be met with coins
-    """
+
     tcoins = 0
     if total <= 0:
         return 0
