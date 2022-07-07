7e5abfd6f94d8d4c6c712b5a38b5de4048ff740f
Ethan-23 <emanroberts@gmail.com>
2022-07-07 21:25:08 +0000

    added helper functions

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 void radix_sort(int *array, size_t size);
+int getMax(int *array, size_t size);
+void sort(int *array, int size, int exp);
 void print_array(const int *array, size_t size);
 
 #endif /* SORT_H */
