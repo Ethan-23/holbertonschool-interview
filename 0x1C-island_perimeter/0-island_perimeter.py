5031354320223c031eae96627383ba2bb733fdc5
Ethan-23 <emanroberts@gmail.com>
2022-07-12 16:40:18 +0000

    added task 1

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""0x1C. Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """counts perimeter of islands in n by n grid"""
+    perim = 0
+    for col in range(len(grid)):
+        print(grid[col])
+        for row in range(len(grid[col])):
+            if grid[col][row] == 1:
+                if grid[col + 1][row] == 0:
+                    perim += 1
+                if grid[col - 1][row] == 0:
+                    perim += 1
+                if grid[col][row + 1] == 0:
+                    perim += 1
+                if grid[col][row - 1] == 0:
+                    perim += 1
+    return(perim)
