6ac8974e29443bab4d9d294af26f3a4339abef24
Ethan <70955616+Ethan-23@users.noreply.github.com>
2022-07-13 14:47:08 -0400

    added correction to length of row and col

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -8,17 +8,17 @@ def island_perimeter(grid):
     for col in range(len(grid)):
         for row in range(len(grid[col])):
             if grid[col][row] == 1:
-                if col < len(grid) and grid[col + 1][row] == 0:
+                if col < len(grid) - 1 and grid[col + 1][row] == 0:
                     perim += 1
-                elif col == len(grid):
+                elif col == len(grid) - 1:
                     perim += 1
                 if col > 0 and grid[col - 1][row] == 0:
                     perim += 1
                 elif col == 0:
                     perim += 1
-                if row < len(grid[col]) and grid[col][row + 1] == 0:
+                if row < len(grid[col]) - 1 and grid[col][row + 1] == 0:
                     perim += 1
-                elif row == len(grid[col]):
+                elif row == len(grid[col]) - 1:
                     perim += 1
                 if row > 0 and grid[col][row - 1] == 0:
                     perim += 1
