f6d6200bd723e953cc8a9f718e2353050bc56387
Ethan-23 <emanroberts@gmail.com>
2022-07-28 02:54:33 +0000

    updated task to not have loo

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -3,43 +3,84 @@
 #include <string.h>
 
 /**
- * wildcmp - check the code for Holberton School students.
+ * passastr - check the code for Holberton School students.
+ * @string: give string
+ * @index: index to start at
+ * Return: index of char after last *.
+ */
+int passastr(char *string, int index)
+{
+	int len = strlen(string);
+
+	if (index < len - 1 && string[index] == '*')
+		return (passastr(string, index + 1));
+	return (index);
+}
+
+/**
+ * checktopletter - check the code for Holberton School students.
+ * @string: given string
+ * @index: starting index for string
+ * @letter: letter to check
+ * @top: top index of given letter
+ * Return: highest index of letter.
+ */
+int checktopletter(char *string, int index, char letter, int top)
+{
+	int len = strlen(string);
+
+	if (string[index] == letter)
+		top = index;
+	if (index == len - 1)
+		return (top);
+	return (checktopletter(string, index + 1, letter, top));
+}
+
+/**
+ * compare_string - check the code for Holberton School students.
  * @s1: string1
  * @s2: string2
- * Return: Always 0.
+ * @i: s1 index
+ * @j: s2 index
+ * Return: Always 1 for same 0 for not.
  */
-int wildcmp(char *s1, char *s2)
+int compare_string(char *s1, char *s2, int i, int j)
 {
-	int i, j = 0;
-	int top = 0;
 	int len1 = strlen(s1);
 	int len2 = strlen(s2);
 
-	for (i = 0; i <= len1; i++)
+	if (s2[j] == '*')
 	{
-		if (s2[j] == '*')
-		{
-			while (s2[j] == '*')
-			{
-				if (j == len2 - 1)
-					return (1);
-				j++;
-			}
-			top = i;
-			while (i < len1)
-			{
-				if (s1[i] == s2[j])
-				{
-					if (top < i)
-						top = i;
-				}
-				i++;
-			}
-			i = top;
-		}
-		if (s1[i] != s2[j])
-			return (0);
-		j++;
+		if (len2 - 1 != j)
+			j = passastr(s2, j);
+
+		if (j == len2 - 1 && s2[j] == '*')
+			return (1);
+		if (i < len1 - 1)
+			i = checktopletter(s1, i, s2[j], i);
 	}
+
+	if (s1[i] != s2[j])
+		return (0);
+
+	if (i < len1 - 1)
+		return (compare_string(s1, s2, i + 1, j + 1));
+
+	if (j < len2 - 1 && s2[passastr(s2, j + 1)] != '*')
+		return (0);
+
 	return (1);
+}
+
+/**
+ * wildcmp - check the code for Holberton School students.
+ * @s1: string1
+ * @s2: string2
+ * Return: 1 for same 0 for different.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int result = compare_string(s1, s2, 0, 0);
+
+	return (result);
 }
--- 0x1E-wild_cmp/holberton.h
+++ 0x1E-wild_cmp/holberton.h
@@ -1,6 +1,9 @@
 #ifndef _WILD_CMP_
 #define _WILD_CMP_
 
+int passastr(char *string, int index);
+int checktopletter(char *string, int index, char letter, int top);
+int compare_string(char *s1, char *s2, int i, int j);
 int wildcmp(char *s1, char *s2);
 
 #endif /* _WILD_CMP_ */
