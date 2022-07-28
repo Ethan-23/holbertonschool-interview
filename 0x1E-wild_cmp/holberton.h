f29d47bf5427aa8596316156aef7718f1e6120b4
Ethan-23 <emanroberts@gmail.com>
2022-07-28 03:01:57 +0000

    added stringlen function

--- 0x1E-wild_cmp/holberton.h
+++ 0x1E-wild_cmp/holberton.h
@@ -1,6 +1,7 @@
 #ifndef _WILD_CMP_
 #define _WILD_CMP_
 
+int stringlen(char *string, int length);
 int passastr(char *string, int index);
 int checktopletter(char *string, int index, char letter, int top);
 int compare_string(char *s1, char *s2, int i, int j);
